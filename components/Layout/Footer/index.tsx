adb43e29e3ad9f7a1bd2626e5dc7f73adc018639
aadityaa2023 <adityamishraniet2023@gmail.com>
2025-08-24 13:32:24 +0530

    footers

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -6,21 +6,19 @@ function Footer() {
   return (
     <footer className="py-10 border-t border-gray-300 dark:border-gray-700 text-start">
       <p className="text-xl lg:text-2xl font-montserrat font-extrabold uppercase">
-        TECHPOST
+       
       </p>
       <p className="mt-2 text-sm">
-        <span className="font-semibold mr-2">Created by:</span>
+        <span className="font-semibold mr-2"></span>
         <Link
-          href="https://niloy.vercel.app"
+          href="#"
           target="_blank"
           className="text-blue-600 dark:text-blue-400 underline link"
         >
-          Ashfak Ahmed Niloy
+        
         </Link>
       </p>
-      <p className="mt-2 text-sm">
-        ©{currentYear}, Ashfak Ahmed Niloy. All rights reserved.
-      </p>
+      
     </footer>
   );
 }
